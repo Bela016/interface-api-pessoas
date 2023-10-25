9e1952c9779f29fe0ef5d04568f33517ab9b19b5
Bela016 <126262449+Bela016@users.noreply.github.com>
2023-10-25 13:03:43 -0300

    atualizado com o html e css

---
+++ js/cadastro.js
@@ -0,0 +1,11 @@
+/*function alerta() {
+    let nome = document.getElementById('exampleFormControlInput1').ariaValueMax;
+
+    if (nome == "")/*vazio {
+        window.alert('voce precisa digitar algo!!');
+
+    } else {
+        window.alert(nome);
+    }
+
+}*/
